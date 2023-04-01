7fb2612ca06547622beaa87924ef95d8bca82547
uzytkownik <manuladysweety7@gmail.com>
2023-04-01 20:10:09 +0200

    Added randomnumbercomputerguess  project.

---
+++ randomnumbercomputerguess.py
@@ -0,0 +1,22 @@
+import random
+
+
+def guess_number(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (h), too low (l), or correct (c)? ')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Congrats! The computer guessed your number! ')
+
+
+guess_number(10)
